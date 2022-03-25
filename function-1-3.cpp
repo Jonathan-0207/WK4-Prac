4392ac665437e5acb1b4d119cbc9226d6db38b75
Jonathan-0207 <100753984+Jonathan-0207@users.noreply.github.com>
2022-03-25 23:46:06 +1030

    Add files via upload

---
+++ function-1-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void cpyia(int old_array[],int new_array[],int length){
+    int *new_ptr, *old_ptr;
+    new_ptr = new_array;
+    old_ptr = old_array;
+
+    for(int j=0; j<length; j++){
+        *(new_ptr + j) = *(old_ptr + j);
+        std::cout << new_ptr[j];
+        if(j<length){
+            std::cout <<" ";
+        }
+    }
+
+}
