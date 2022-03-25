3e5d8dc21e8d784bc89ef6fa4e04f6c4f754fd71
Jonathan-0207 <100753984+Jonathan-0207@users.noreply.github.com>
2022-03-25 23:39:10 +1030

    Add files via upload

---
+++ main-1-3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+void cpyia(int old_array[],int new_array[],int length);
+
+int main(){
+    int a1[15] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+    int a2[15];
+    cpyia(a2, a1, 15);
+
+}
+
